ce4cfa8b2942338428343a4a3e862e279b82937e
deblaseroman <79052824+deblaseroman@users.noreply.github.com>
2024-09-08 20:28:01 -0400

    Initial logic for calculating tip using ternary operator

---
+++ bill_splitter.js
@@ -0,0 +1,13 @@
+
+
+//Initial logic for calculating tip using ternary operator
+let bill = 222;
+let tip;
+    if (bill >= 50 && bill <= 300) {
+        tip = bill * 0.15;
+    } else {
+        tip = bill * 0.2;
+    }
+
+    
+
