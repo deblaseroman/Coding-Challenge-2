33a136cbbbc6c3e431331d6a2ff14a60b90b3316
deblaseroman <79052824+deblaseroman@users.noreply.github.com>
2024-09-08 21:47:16 -0400

    creating the function

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,8 +1,9 @@
-
+//creating the function
+function calculateTip(bill){
 
 //Initial logic for calculating tip using ternary operator
-let bill = 222;
-let tip;
+let bill = 100;
+var tip;
     if (bill >= 50 && bill <= 300) {
         tip = bill * 0.15;
     } else {
@@ -12,3 +13,5 @@ let tip;
     let total = (bill + tip);
 //Output Details
 console.log ("The bill was:", bill,"the tip was:", tip,"and the total value was:", total)
+
+}
